aa8c13164854eed7186c0650639cc07820b6c16b
usrwatermelon <kyntltntn@protonmail.com>
2021-10-12 15:34:09 +0000

    Constructed modules for gathering weather data from OpenWeatherAPI and outlined markdown

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 import "./styles.css";
 import "./reset.css";
-console.log("hi");
+import regeneratorRuntime from "regenerator-runtime";
+
+async function getWeather(city){
+    try {
+        const weatherData = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=36304148de798623bfc32544c736df2a`);
+        const weatherJson =  await weatherData.json();
+        console.log(weatherJson);
+        let temperature = weatherJson['main']['temp'];
+        let feelsLike = weatherJson['main']['feels_like'];
+        let humidity = weatherJson['main']['humidity'];
+        displayWeather(temperature, feelsLike, humidity);
+    }
+    catch(error) {
+        alert(error);
+    }
+    
+}
+
+const inputLocation =(() => {
+    const inputLoc = document.querySelector('.search-loc');
+    const submitBtn = document.querySelector('.submit-loc');
+
+    submitBtn.addEventListener('click', () => getWeather(inputLoc.value));
+})();
+
+const displayWeather = (temp, fL, hum) => {
+    const tempOutput = document.querySelector('.temperature');
+    const fLOutput = document.querySelector('.feels-like');
+    const humidity = document.querySelector('.humidity');
+
+    tempOutput.textContent = temp;
+    fLOutput.textContent = fL;
+    humidity.textContent = hum;
+}
+
+
+
+
+
+
